ee820b5ad23eb439f6773b9b3313de9f7ceb4558
Jason Barnabe <jason.barnabe@gmail.com>
2025-12-29 20:01:21 -0600

    Update manual type field based on classification

--- app/javascript/src/item_subcategory_picker.js
+++ app/javascript/src/item_subcategory_picker.js
@@ -9,4 +9,21 @@ document.addEventListener('turbo:frame-load', () => {
       frame.src = url.toString()
     })
   }
+
+  let classificationPickers = document.querySelectorAll('.classification-picker a')
+  classificationPickers.forEach((picker) => {
+    picker.addEventListener('click', (event) => {
+      let showManualType = picker.dataset.classification == 'equipment'
+
+      let manualType = document.querySelector('.manual-type')
+      if (showManualType) {
+        manualType.classList.remove('not-needed')
+      } else {
+        manualType.classList.add('not-needed')
+      }
+      manualType.querySelectorAll('select').forEach((input) => {
+        input.disabled = !showManualType
+      })
+    })
+  })
 })
