209b2f0d4fe124bc4cdd1f57749a6e93f09947d5
Jason Barnabe <jason.barnabe@gmail.com>
2026-01-15 13:48:45 -0600

    Adjust picked_by_id initial value

--- db/migrate/20260115192311_add_picked_by_user_id_to_inventory_items.rb
+++ db/migrate/20260115192311_add_picked_by_user_id_to_inventory_items.rb
@@ -2,6 +2,6 @@ class AddPickedByUserIdToInventoryItems < ActiveRecord::Migration[8.1]
   def change
     add_column :inventory_items, :picked_by_id, :integer
     add_foreign_key :inventory_items, :users, column: :picked_by_id
-    execute 'UPDATE inventory_items SET picked_by_id = 1 WHERE container_id IS NOT NULL'
+    execute 'UPDATE inventory_items SET picked_by_id = (SELECT id from users order by id limit 1) WHERE container_id IS NOT NULL'
   end
 end
