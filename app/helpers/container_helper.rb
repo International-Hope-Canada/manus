9cd5659ef174bc9e2261d9fb611a5cd12f0925c0
Jason Barnabe <jason.barnabe@gmail.com>
2026-02-02 10:29:09 -0600

    Adjust certificate of donation and packing list

--- app/helpers/container_helper.rb
+++ app/helpers/container_helper.rb
@@ -1,12 +1,12 @@
 module ContainerHelper
   def container_items_for_printable(container)
-    container.inventory_items.includes(item_subcategory: :item_category).order(:classification, item_category: { name: :asc }, item_subcategory: { name: :asc }, barcode: :asc)
+    container.inventory_items.includes(item_subcategory: :item_category).order(classification: :desc, item_category: { name: :asc }, item_subcategory: { name: :asc }, barcode: :asc)
   end
 
   def summarized_container_items_for_printable(container)
     items = container_items_for_printable(container)
-    items.group_by { |item| item.item_subcategory }.map do |subcategory, items_in_category|
-      [ subcategory, items_in_category.count, items_in_category.sum(&:value) ]
+    items.group_by { |item| item.supply? ? :supply : item.item_category.name }.map do |category, items_in_category|
+      [ category, items_in_category.count, items_in_category.sum(&:value) ]
     end
   end
 end
