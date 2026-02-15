512851884fceabc53f35c7be19d9f2fe5addb75a
Jason Barnabe <jason.barnabe@gmail.com>
2026-02-15 16:10:16 -0600

    Bigger text size for login and home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authorize_user!
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  #allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
